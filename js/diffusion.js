0dc4f2bf70324e893264c41782ad08fea1d01ede
Chorus <wktsuiaa@connect.ust.hk>
2019-05-05 14:19:23 +0800

    diffusion

--- js/diffusion.js
+++ js/diffusion.js
@@ -82,9 +82,20 @@ imageproc.diffusion = function(inputData, outputData){
 		let temp = val+newVal;
 		if(temp<0) return 0;
 		if (temp>255) return 255;
-		return temp;
+		return parseInt(temp);
 	}
 
+	for (var y = 0; y < inputData.height; y++) {
+        for (var x = 0; x < inputData.width; x++) {
+        	var rgb = imageproc.getPixel(inputData, x, y);
+        	let i = ( (x) + (y * outputData.width)) * 4;
+
+        	outputData.data[i] = rgb.r;
+        	outputData.data[i+1] = rgb.g;
+        	outputData.data[i+2] = rgb.b;
+
+        }
+    }
 
 
 	for (var y = 0; y < inputData.height; y++) {
@@ -108,36 +119,38 @@ imageproc.diffusion = function(inputData, outputData){
 
             if( x +1 <inputData.width){
             	let i = ( (x+1) + (y * outputData.width)) * 4;
-            	outputData[i] = checkValid(outputData[i],(errorR*7)/16);
-            	outputData[i+1] = checkValid(outputData[i+1],(errorG*7)/16);
-            	outputData[i+2] = checkValid(outputData[i+2],(errorB*7)/16);
+
+            	outputData.data[i] = checkValid(outputData.data[i],(errorR*7)/16);
+            	outputData.data[i+1] = checkValid(outputData.data[i+1],(errorG*7)/16);
+            	outputData.data[i+2] = checkValid(outputData.data[i+2],(errorB*7)/16);
             	
             }
 
             if( x-1 > 0 && y+1 < inputData.height){
             	let i = ((x-1) + ((y+1) * outputData.width)) * 4;
-            	outputData[i] = checkValid(outputData[i],(errorR*3)/16);
-            	outputData[i+1] = checkValid(outputData[i+1],(errorG*3)/16);
-            	outputData[i+2] = checkValid(outputData[i+2],(errorB*3)/16);
+            	outputData.data[i] = checkValid(outputData.data[i],(errorR*3)/16);
+            	outputData.data[i+1] = checkValid(outputData.data[i+1],(errorG*3)/16);
+            	outputData.data[i+2] = checkValid(outputData.data[i+2],(errorB*3)/16);
             }
 
             if( y+1 < inputData.height){
             	let i = (x + ((y+1) * outputData.width)) * 4;
-            	outputData[i] = checkValid(outputData[i],(errorR*5)/16);
-            	outputData[i+1] = checkValid(outputData[i+1],(errorG*5)/16);
-            	outputData[i+2] = checkValid(outputData[i+2],(errorB*5)/16);
+            	outputData.data[i] = checkValid(outputData.data[i],(errorR*5)/16);
+            	outputData.data[i+1] = checkValid(outputData.data[i+1],(errorG*5)/16);
+            	outputData.data[i+2] = checkValid(outputData.data[i+2],(errorB*5)/16);
             }
 
 
             if( x+1 < inputData.width && y+1 < inputData.height){
             	let i = (x+1 + ((y+1) * outputData.width)) * 4;
-            	outputData[i] = checkValid(outputData[i],errorR/16);
-            	outputData[i+1] = checkValid(outputData[i+1],errorG/16);
-            	outputData[i+2] = checkValid(outputData[i+2],errorB/16);
+            	outputData.data[i] = checkValid(outputData.data[i],errorR/16);
+            	outputData.data[i+1] = checkValid(outputData.data[i+1],errorG/16);
+            	outputData.data[i+2] = checkValid(outputData.data[i+2],errorB/16);
             }
 
 
 			}
+
 	}
 
 
